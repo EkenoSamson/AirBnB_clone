e51fdd9d9d056fd6f4c90dd516552a1479453bf9
EkenoSamson <samsonekeno98@gmail.com>
2024-03-06 04:31:50 -0800

    create base_model.py

---
+++ models/base_models.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module contains the parent class BaseModel"""
+import uuid
+import datetime
+
+
+class BaseModel(object):
+    """class that defines all common attributes/methods for other classes"""
+    def __init__(self):
+        """declares and initialise instance variables"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now
+        self.updated_at = datetime.now
+
+
