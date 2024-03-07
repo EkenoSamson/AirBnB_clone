dbf2ffe727c404129408cb88e6cb44ba281cf7c3
EkenoSamson <samsonekeno98@gmail.com>
2024-03-07 05:09:19 -0800

    update base_model

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" module contains the parent class BaseModel"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel(object):
+    """class that defines all common attributes/methods for other classes"""
+    def __init__(self):
+        """declares and initialise instance variables"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ print: [<class name>] (<self.id>) <self.__dict__>"""
+        return ("[{}] ({}) {}>".format(self.__class__.__name__,
+                self.id, self.__dict__))
+
+    def save(self):
+        """update the current updated_at"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ """
+        dict = self.__dict__.copy()
+        dict["__class__"] = self.__class__.__name___
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+
+        return dict
