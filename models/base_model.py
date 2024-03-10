ff324e03d07bb63f5ee40e071685d4ea9fd2cb29
EkenoSamson <samsonekeno98@gmail.com>
2024-03-10 01:54:28 -0800

    add test for base_model

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,23 @@ from datetime import datetime
 
 class BaseModel(object):
     """class that defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """declares and initialise instance variables"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.fromisoformat(value)
+                else:
+                    setattr(self, key, value)
+        else:                
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ print: [<class name>] (<self.id>) <self.__dict__>"""
